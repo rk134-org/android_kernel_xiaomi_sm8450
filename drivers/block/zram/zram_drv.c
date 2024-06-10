5bd9fd15bcf6a2aa98a0221e5d8dce64ecebc1fb
rahul <rahul@aospa.co>
2024-06-10 20:34:27 +0000

    drivers/block/zram: Revert all changes
    Revert "zram: do not waste zram_table_entry flags bits"
    
    This reverts commit 21ea94eacbb702b7d6b41b07a64c2aa683df853a.
    
    Revert "zram_drv: allow reclaim on bio_alloc"
    
    This reverts commit 866e36fe0ef5d0bda78ff2ecbccdd25006cd3e67.
    
    Revert "zram: replace fsync_bdev with sync_blockdev"
    
    This reverts commit 59eb7fd135965fc72e366fdfef0db080cc739915.
    
    Revert "zram: fix race between zram_reset_device() and disksize_store()"
    
    This reverts commit 73487f40db8a169def66e014732b6480471b9049.

--- drivers/block/zram/zram_drv.c
+++ drivers/block/zram/zram_drv.c
@@ -597,7 +597,7 @@ static int read_from_bdev_async(struct zram *zram, struct bio_vec *bvec,
 {
 	struct bio *bio;
 
-	bio = bio_alloc(GFP_NOIO, 1);
+	bio = bio_alloc(GFP_ATOMIC, 1);
 	if (!bio)
 		return -ENOMEM;
 
@@ -1717,13 +1717,12 @@ static void zram_reset_device(struct zram *zram)
 	set_capacity(zram->disk, 0);
 	part_stat_set_all(&zram->disk->part0, 0);
 
+	up_write(&zram->init_lock);
 	/* I/O operation under all of CPU are done so let's free */
 	zram_meta_free(zram, disksize);
 	memset(&zram->stats, 0, sizeof(zram->stats));
 	zcomp_destroy(comp);
 	reset_bdev(zram);
-
-	up_write(&zram->init_lock);
 }
 
 static ssize_t disksize_store(struct device *dev,
@@ -1808,7 +1807,7 @@ static ssize_t reset_store(struct device *dev,
 	mutex_unlock(&bdev->bd_mutex);
 
 	/* Make sure all the pending I/O are finished */
-	sync_blockdev(bdev);
+	fsync_bdev(bdev);
 	zram_reset_device(zram);
 	revalidate_disk_size(zram->disk, true);
 	bdput(bdev);
@@ -2016,7 +2015,7 @@ static int zram_remove(struct zram *zram)
 	zram_debugfs_unregister(zram);
 
 	/* Make sure all the pending I/O are finished */
-	sync_blockdev(bdev);
+	fsync_bdev(bdev);
 	zram_reset_device(zram);
 	bdput(bdev);
 
@@ -2117,8 +2116,6 @@ static void destroy_devices(void)
 static int __init zram_init(void)
 {
 	int ret;
-
-	BUILD_BUG_ON(__NR_ZRAM_PAGEFLAGS > BITS_PER_LONG);
 
 	ret = cpuhp_setup_state_multi(CPUHP_ZCOMP_PREPARE, "block/zram:prepare",
 				      zcomp_cpu_up_prepare, zcomp_cpu_dead);
