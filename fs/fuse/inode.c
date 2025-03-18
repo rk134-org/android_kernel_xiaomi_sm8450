0ab03cf19dff3b26b19d2753c2d936d6677faae4
Rahul <rahul@aospa.co>
2025-03-18 23:34:49 +0000

    kernel: Fix compilation errors

--- fs/fuse/inode.c
+++ fs/fuse/inode.c
@@ -152,7 +152,6 @@ static void fuse_evict_inode(struct inode *inode)
 static void fuse_destroy_inode(struct inode *inode)
 {
 	struct fuse_inode *fi = get_fuse_inode(inode);
-
 	iput(fi->backing_inode);
 }
 #endif
@@ -760,14 +759,14 @@ static int fuse_parse_param(struct fs_context *fsc, struct fs_parameter *param)
 						BPF_PROG_TYPE_FUSE, false);
 		if (IS_ERR(ctx->root_bpf)) {
 			ctx->root_bpf = NULL;
-			return invalfc(fc, "Unable to open bpf program");
+			return invalfc(fsc, "Unable to open bpf program");
 		}
 		break;
 
 	case OPT_ROOT_DIR:
 		ctx->root_dir = fget(result.uint_32);
 		if (!ctx->root_dir)
-			return invalfc(fc, "Unable to open root directory");
+			return invalfc(fsc, "Unable to open root directory");
 		break;
 
 	case OPT_NO_DAEMON:
